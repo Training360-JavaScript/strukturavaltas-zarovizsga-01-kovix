cfeaa34161de350e02d58b623581d722f217d14d
Daniel Kovacs <info@kovix.info>
2022-02-28 14:18:35 +0100

    1.feladat 2.resz ready

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,15 @@
+/* Megkeres az oldalon minden quote osztályhoz tartozó elemet, és három stílust állít be rájuk JavaScript segítségével:
+a körvonaluk legyen kék,
+a hátterük világoskék
+és az általuk használt betűtípus dőlt. */
 function manipulateDom() {
-    // 
+    const quotes = Array.from(document.querySelectorAll('.quote'));
+    quotes.forEach(quote => {
+        quote.style.borderColor = 'blue';
+        quote.style.backgroundColor = 'lightblue';
+        quote.style.fontStyle = 'italic';
+    });
+
 };
 
 export { manipulateDom };

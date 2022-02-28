063517aef5de996f820feb7cd02de23f193540f0
Daniel Kovacs <info@kovix.info>
2022-02-28 15:44:28 +0100

    added list and delete logic to app component, all test passes

--- angular-3/src/app/app.component.ts
+++ angular-3/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Construction } from './model/construction';
+import { ConstructionService } from './service/construction.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-3';
+
+  list$: Observable<Construction[]> = this.constructionService.getAll();
+
+  constructor(
+    private constructionService: ConstructionService
+  ) {}
+
+  onDelete(construction: Construction) {
+    this.constructionService.delete(construction).forEach(response => {
+      this.list$ = this.constructionService.getAll();
+    });
+  }
 
 }

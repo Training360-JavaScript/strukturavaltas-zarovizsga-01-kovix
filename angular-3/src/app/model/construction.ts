c7fa4468e76d194c02cae2c45ffc7f2fd0c81124
Daniel Kovacs <info@kovix.info>
2022-02-28 15:25:28 +0100

    model ready (construction)

---
+++ angular-3/src/app/model/construction.ts
@@ -0,0 +1,15 @@
+// "id": 48,
+// "material": "Wood",
+// "trade": "Boilermaker",
+// "cost_code": "6-900 - Wood and Plastic Restoration and Cleaning",
+// "price": 2211,
+// "active": false
+
+export class Construction {
+  id: number = 0;
+  material: string = "";
+  trade: string = "";
+  cost_code: string = "";
+  price: number = 0;
+  active: boolean = false;
+}

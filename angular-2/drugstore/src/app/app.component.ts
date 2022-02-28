5e2b357d92a7106c6e4bfdb54f4102bf5959c190
Daniel Kovacs <info@kovix.info>
2022-02-28 15:14:50 +0100

    app.component add comments

--- angular-2/drugstore/src/app/app.component.ts
+++ angular-2/drugstore/src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Observable } from 'rxjs';
 import { Drug } from './model/drug';
 import { DrugService } from './service/drug.service';
 
+
+/**
+ * Warning!!!!
+ * Ezeket a sorokat azért tettem bele, mert a 2. feladat 1. rész tartalmazott egy ilyen kitételt:
+ * A munkád manuális ellenőrzéséhez az app.component.ts-be injektáld be a szervizedet, és írasd ki a konzolra a gyógyszerek listáját.
+ * Ugyanakkor így az scss teszt nem fut le, csak ha a DrugService nincs beinjektálva.
+ * az scss vizuálisan ettől még jónak tűnik, illetve ha alaphelyzetbe állítom a controller kódot, akkor a teszt is lefut.
+ * KD
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,14 +21,18 @@ import { DrugService } from './service/drug.service';
 export class AppComponent implements OnInit {
   title = 'drugstore';
 
-  drugList$: Observable<Drug[]> = this.drugService.getAll();
+  drugList$: Observable<Drug[]> = this.drugService.getAll(); //remove this for test:23 scss
 
+  //remove this for test:23 scss
   constructor(
     private drugService: DrugService
   ) { }
-
+  //remove this for test:23 scss
   ngOnInit(): void {
       this.drugList$.subscribe(drugs => console.log(drugs));
   }
+
+  //constructor() { }
+  //ngOnInit() { }
 
 }
